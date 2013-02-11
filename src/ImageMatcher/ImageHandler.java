50bc94f524999e285c4f0081040128420b6e4924
RobToombs <toombs.rob@gmail.com>
2013-02-10 22:58:29 -0500

    Added file validation and conversion to BufferedImage
    Before attempting to use the BufferedImage object from the ImageHandler,
    verify that the image is valid by calling isValidImg. Discard the ImageHandler
    upon an invalid image

--- src/ImageMatcher/ImageHandler.java
+++ src/ImageMatcher/ImageHandler.java
@@ -1,27 +1,117 @@
 package ImageMatcher;
 
+import java.awt.image.BufferedImage;
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 
-//Loads & Checks images from filepaths
-//Also checks the format
+/**
+ * Handles File to BufferedImage validation and conversion
+ * 
+ * @author RobToombs
+ *
+ */
 public class ImageHandler{
 	
-	//Should verify image and throw exception if not valid
-	public ImageHandler(File imagePath){
+	private BufferedImage image = null;     // Handled Image
+	private boolean validImg = false;       // Is Handled Image Valid?
+	
+	// List of valid image types (MUST BE LOWERCASE!)
+	private ArrayList<String> VALID_TYPES = new ArrayList<String>() {{
+		add("jpeg");
+		add("png");
+		add("gif");
+	}};
+	
+	/**
+	 * Constructor
+	 * 
+	 * Handles File Validation/Initializes Buffered Image
+	 *  
+	 * @param imageFile Image File
+	 */
+	public ImageHandler(File imageFile){
+
+			// Open Image Reading Streams
+			try {
+				FileInputStream	fis = new FileInputStream(imageFile);	
+				BufferedInputStream bis = new BufferedInputStream(fis);
+				ImageInputStream iis = ImageIO.createImageInputStream(bis);
 		
+				// Grab the appropriate image reader for the input stream
+				Iterator<ImageReader> iter = ImageIO.getImageReaders(iis);
+				
+				// If it is a support ImageIO reader type (JPEG, GIF, PNG, BMP, WBMP)
+				if(iter.hasNext()) {
+					ImageReader reader = (ImageReader) iter.next();
+					reader.setInput(iis);
+					String fileType = reader.getFormatName().toLowerCase();
+
+					// Validate reader type
+					if(VALID_TYPES.contains(fileType)) {
+						image = reader.read(0);				
+						validImg = true;
+					} else {
+						System.out.println("Invalid image type @ " + imageFile.getAbsolutePath());
+					}
+				} else {
+					System.out.println("Invalid image type @ " + imageFile.getAbsolutePath());
+				}
+				
+				// Close the input streams
+				iis.close();
+				bis.close();
+				fis.close();
+			} catch (FileNotFoundException e) {
+				System.out.println("Unable to find file @ " + imageFile.getAbsolutePath());
+				e.printStackTrace();
+			} catch (IOException e) {
+				System.out.println("Error reading file @ " + imageFile.getAbsolutePath());
+				e.printStackTrace();
+			}
+
 	}
 
+	/**
+	 * Use this to verify that the image being handled is existent/valid before using it
+	 * 
+	 * @return Does this image handle contain a valid image?
+	 */
+	public boolean isValidImg() {
+		return validImg;
+	}
 	
-	//Pull image from path
+	public void setValidImg(boolean validImg) {
+		this.validImg = validImg;
+	}
 		
-	//Determine what Image type it is 
-		
-	//Verify Handle if it is not an image
-		
-	//only deals with one image at a time
-		
-	//get - set image components
-	
-		
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+	/**
+	 * FOR TESTING PURPOSES, CAN REMOVE AT ANY TIME
+	 *  
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		File file = new File(args[0]);
+		ImageHandler handler = new ImageHandler(file);
+		if(handler.isValidImg()) {
+			BufferedImage img = handler.getImage();
+			System.out.println("IMAGE WIDTH x HEIGHT DIMENSIONS -- " + img.getWidth() + " x " + img.getHeight());
+		}
+	}
 }
