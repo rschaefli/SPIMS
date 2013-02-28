6cfe651c1e4a95486dc52d344cebde567c3a309c
RobToombs <toombs.rob@gmail.com>
2013-02-27 20:31:05 -0500

    updated default thread count

--- src/ImageMatcher/SPIMS.java
+++ src/ImageMatcher/SPIMS.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class SPIMS {
 
 	// Specifies the desired number of comparison threads
-	private final static int NUM_THREADS = 1;
+	private final static int NUM_THREADS = 4;
 	
 	public static void main(String[] args) {
 
